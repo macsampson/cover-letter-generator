70e1dcebf54376359ac1df4e4493987ac0dffd6e
Mac <mac.sampson@pm.me>
2023-03-20 18:05:47 -0700

    remove saving file to disk, only read stream

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,13 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import formidable from 'formidable'
-import pdfParse from 'pdf-parse'
-import fs from 'fs'
-
+// import pdfParse from 'pdf-parse'
+// import fs from 'fs'
+// import multer from 'multer'
+// import nextConnect from 'next-connect'
+// import WordExtractor from 'word-extractor'
 import { Configuration, OpenAIApi } from 'openai'
 
 const reader = require('any-text')
 
 require('dotenv').config()
+
+// interface BufferedFile extends File {
+// 	buffer: Buffer
+// }
+
+// declare module 'next' {
+// 	export interface NextApiRequest {
+// 		file: BufferedFile
+// 	}
+// }
 
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -63,18 +75,87 @@ export const config = {
 	},
 }
 
+// const upload = multer({
+// 	storage: multer.memoryStorage(),
+// 	limits: { fileSize: 1e6 },
+// 	fileFilter: (req, file, cb) => {
+// 		if (
+// 			file.mimetype !==
+// 			'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+// 		) {
+// 			return cb(new Error('Invalid file type'))
+// 		}
+// 		return cb(null, true)
+// 	},
+// })
+
+// const apiRoute = nextConnect({
+// 	// Handle any other HTTP method
+// 	onNoMatch(req: NextApiRequest, res: NextApiResponse) {
+// 		res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
+// 	},
+// })
+
+// apiRoute.use(upload.single('file'))
+
+// apiRoute.post(async (req: NextApiRequest, res: NextApiResponse) => {
+// 	const file = req.body
+// 	try {
+// 		if (!file) {
+// 			res.status(400).send('No file uploaded.')
+// 			return
+// 		}
+// 		// const extractor = new WordExtractor()
+// 		// const text = await extractor.extract(file.buffer)
+// 		console.log(file)
+
+// 		res.status(200).send('File uploaded.')
+// 	} catch (error) {
+// 		console.error(error)
+// 		res.status(500).send('Error uploading file.')
+// 	}
+// })
+
+// export default apiRoute
+
 // Export the API route
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	// Make sure the method is POST
 	if (req.method !== 'POST') {
 		res.status(405).end()
 		return
 	}
-	const options = { uploadDir: './uploads', keepExtensions: true }
-	const form = formidable(options)
+
+	const form = formidable.formidable({ keepExtensions: true })
+
+	// 	upload.single('file')(req, res, (err: any) => {
+	// 		if (err) {
+	// 			console.error(err)
+	// 			res.status(500).send('Error uploading file.')
+	// 			return
+	// 		}
+
+	// 		const file = req.file
+
+	// 		try {
+	// 			if (!file) {
+	// 				res.status(400).send('No file uploaded.')
+	// 				return
+	// 			}
+
+	// 			// assuming the file is a text document
+	// 			const text = file.buffer.toString('utf-8')
+	// 			console.log(text)
+
+	// 			res.status(200).send('File uploaded.')
+	// 		} catch (error) {
+	// 			console.error(error)
+	// 			res.status(500).send('Error uploading file.')
+	// 		}
+	// 	})
+	// }
 
 	form.parse(req, async (err, fields, files) => {
 		if (err) {
@@ -96,13 +177,7 @@ export default async function handler(
 			const data = await reader.getText((file as any).filepath)
 			// console.log(data) // handle success
 			const response = await createCoverLetter(data, fields.text)
-			try {
-				fs.unlinkSync((file as any).filepath)
-				//file removed
-			} catch (err) {
-				console.error(err)
-			}
-			// console.log(response) // handle success
+
 			if (response) {
 				res.status(200).json({ message: response })
 				return
